55b7c36e2b4b4c498de360c601b0bba83c356a52
Endre Márk Borza <endremborza@gmail.com>
2020-11-05 16:39:16 +0100

    cleanup tasks.py

--- tasks.py
+++ tasks.py
@@ -11,11 +11,3 @@ for pe in PipelineElement.all_instances():
 def clean(c):
     for _pe in PipelineElement.all_instances():
         c.run(f"rm -rf {_pe.output_path}")
-
-"""
-dvc run -n train \
-          -p train.seed, train.n_estimators \
-          -d src/train.py -d data/features \
-          -o model.pkl \
-          python src/train.py data/features model.pkl
-"""
