0373dc6e82db18e84431de722a79f87800835a2e
Endre Márk Borza <endremborza@gmail.com>
2020-11-05 17:31:36 +0100

    add dag task

--- tasks.py
+++ tasks.py
@@ -11,3 +11,9 @@ for pe in PipelineElement.all_instances():
 def clean(c):
     for _pe in PipelineElement.all_instances():
         c.run(f"rm -rf {_pe.output_path}")
+
+
+@task
+def dag(c):
+    c.run("dvc dag --dot > dvcdag.dot")
+    c.run("dot -Tpng dvcdag.dot -o dvcdag.png")
