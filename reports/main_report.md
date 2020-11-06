df14d705b6b5d465af4e8a14b1c58bafe701ffa6
Endre Márk Borza <endremborza@gmail.com>
2020-11-06 01:54:38 +0100

    reporting init

---
+++ reports/main_report.md
@@ -0,0 +1,18 @@
+# This is a frame for filling
+
+`final_report` dvc step creates this report 
+and all figures and tables used by it
+
+## Model
+
+
+## Network
+
+
+## Style
+
+
+## Success
+
+
+## 
---
+++ src/final_report.py
@@ -0,0 +1,15 @@
+from .dvc_util import PipelineElement
+from .pass_success_model.evaluate_model import eval_pass_model_pe
+anal_networks_pe = ...
+anal_styles_pe = ...
+
+
+def export_report():
+    ...
+
+
+report_pe = PipelineElement(
+    name="final_report",
+    runner=export_report,
+    dependency_list=[eval_pass_model_pe, anal_networks_pe, anal_styles_pe],
+)
